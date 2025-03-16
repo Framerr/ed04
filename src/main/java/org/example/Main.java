7182a043af46d5f362adf9d142d29816d4f3ef86
Framerr <4186837@alu.murciaeduca.es>
2025-03-16 18:58:35 +0100

    Introducción del método initAgenda en Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Crear una nueva agenda.
         Agenda agendaContactos = new Agenda();
-
-        // Añadir contactos a la agenda.
-        agendaContactos.addContact("John Doe", "1234567890");
-        agendaContactos.addContact("Jane Doe", "9876543210");
-        agendaContactos.addContact("Mary Jane", "1122334455");
+        initAgenda(agendaContactos);
 
         // Imprimir todos los contactos de la agenda.
         System.out.println("Todos los contactos: ");
@@ -16,18 +12,19 @@ public class Main {
             System.out.println(c.getName() + " -> " + c.getPhones());
         }
 
-        // Añadir un nuevo teléfono a un contacto existente.
-        agendaContactos.addContact("John Doe", "6543219876");
-
-        // Eliminar un contacto de la agenda.
-        agendaContactos.removeContact("Jane Doe");
-
-        // Modificar el teléfono de un contacto existente en la agenda.
-        agendaContactos.modifyPhoneNumber("John Doe", "1234567890", "6666666666");
-
-        System.out.println("\nDespués de añadir, eliminar y modificar contactos: ");
+        System.out.println("\nDespués de operaciones: ");
         for (Contacto c : agendaContactos.getContacts()) {
             System.out.println(c.getName() + " -> " + c.getPhones());
         }
+    }
+
+    // Añadir, Eliminar y Modificar contactos de la agenda.
+    private static void initAgenda(Agenda agenda) {
+        agenda.addContact("John Doe", "1234567890");
+        agenda.addContact("Jane Doe", "9876543210");
+        agenda.addContact("Mary Jane", "1122334455");
+        agenda.addContact("John Doe", "6543219876");
+        agenda.removeContact("Jane Doe");
+        agenda.modifyPhoneNumber("John Doe", "1234567890", "6666666666");
     }
 }
